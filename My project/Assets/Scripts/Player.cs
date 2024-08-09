706aec5625f0cd4bb684fde6002694b01325c4a5
yasmim <yasmim.souza@escolar.ifrn.edu.br>
2024-08-09 10:10:39 -0300

    dia 09/08

---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Input = UnityEngine.Windows.Input;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+       
+    }
+}
