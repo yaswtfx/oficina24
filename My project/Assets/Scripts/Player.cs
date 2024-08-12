35e6d6a303f4bcc5ca7ffb5c5f544c056860d308
yasmim <yasmim.souza@escolar.ifrn.edu.br>
2024-08-12 08:19:43 -0300

    dia 12/08

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -1,25 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Input = UnityEngine.Windows.Input;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
-    // Start is called before the first frame update
+    public float Speed;
+    public float jumpForce;
+
+    public bool isJumping;
+    public bool doubleJump;
+
+    private Rigidbody2D rig;
+
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Move();
+       Move();
+       Jump();
     }
 
     void Move()
     {
-       
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * Speed;
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+           if(!isJumping)
+           {
+                rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+           }
+
+           else
+           {
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+           }
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = true;
+        }
     }
 }
