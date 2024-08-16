dc1f771f269d68ebec0d47370be10f7498c7cdbf
yasmim <yasmim.souza@escolar.ifrn.edu.br>
2024-08-16 09:35:22 -0300

    dia 16/08 part 2/2

--- My project/Assets/Scripts/Cherrie.cs
+++ My project/Assets/Scripts/Cherrie.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class Cherrie : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    private CircleCollider2D circle;
+
+    public GameObject collected;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        sr = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.gameObject.tag == "Player")
         {
-            Destroy(gameObject);
+            sr.enabled = false;
+            circle.enabled = false;
+            collected.SetActive(true);
+            Destroy(gameObject, 0.25f);
         }
     }
 }
