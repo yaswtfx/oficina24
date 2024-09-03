3da2d6a999be66df494bc20ab2d73526da52d2f5
yasmim <yasmim.souza@escolar.ifrn.edu.br>
2024-09-03 10:25:47 -0300

    video 3 - 03/09

--- My project/Assets/Cutscene/Scripts/EfeitoDigitador.cs
+++ My project/Assets/Cutscene/Scripts/EfeitoDigitador.cs
@@ -11,28 +11,36 @@ public class NewBehaviourScript : MonoBehaviour
     private AudioSource _audioSource;
     private string mensagemOriginal;
     public bool imprimindo;
+    public float tempoEntreLetras = 0.08f;
 
     void Awake()
     {
         TryGetComponent(out componenteTexto);
         TryGetComponent(out _audioSource);
         mensagemOriginal = componenteTexto.text;
-        componenteTexto = "";
+        componenteTexto.text = "";
+    
     }
 
     void OnEnable()
     {
-
+        ImprimirMensagem(mensagemOriginal);
     }
 
     void OnDisable()
     {
         componenteTexto.text = mensagemOriginal;
+        StopAllCoroutines();
     }
 
     public void ImprimirMensagem(string mensagem)
-    {
-
+    {    
+        if(gameObject.activeInHierarchy)
+        {
+            if(imprimindo) return;
+            imprimindo = true;
+            StartCoroutine(LetraPorLetra(mensagem));
+        }
     }
 
     IEnumerator LetraPorLetra(string mensagem)
@@ -42,7 +50,12 @@ public class NewBehaviourScript : MonoBehaviour
         foreach(var letra in mensagem)
         {
             msg += letra;
-            componenteTexto.text = mensagem;
+            componenteTexto.text = msg;
+            _audioSource.Play();
+            yield return new WaitForSeconds(tempoEntreLetras);
         }
+
+        imprimindo = false;
+        StopAllCoroutines();
     }
 }
